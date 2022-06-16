094abcf48837ea312c52eb18fb8e8d7145b31f52
金小贱 <786518239@qq.com>
2022-06-16 14:47:33 +0800

    文件操作，了解 with open 后可操作几次文件；（只能操作一次）

---
+++ 日常练习/文件操作.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Filename:jinxiaojian
+# Create Time: 2022/6/16 14:22
+
+with open('test.txt', 'a', encoding="utf-8") as f:
+    # content = f.read()    # 操作一次就关闭
+    f.write("/写入内容")
+    # content2 = f.read()
+    #
+    # print("第一次读取" + content)
+    # print("第二次读取" + content2)
