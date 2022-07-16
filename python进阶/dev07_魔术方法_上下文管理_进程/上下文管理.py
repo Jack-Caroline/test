b552b9cf454625b89f392427f4c5b4c60318879d
jack <786518239@qq.com>
2022-07-17 00:14:19 +0800

    进程基本知识

--- python进阶/dev07_魔术方法_上下文管理_进程/上下文管理.py
+++ python进阶/dev07_魔术方法_上下文管理_进程/上下文管理.py
@@ -2,3 +2,6 @@
 # -*- coding: UTF-8 -*-
 # Filename:jinxiaojian
 # Create Time: 2022/7/16 下午6:57
+
+with open() as f:
+    f.read()
--- python进阶/dev07_魔术方法_上下文管理_进程/进程.py
+++ python进阶/dev07_魔术方法_上下文管理_进程/进程.py
@@ -2,3 +2,37 @@
 # -*- coding: UTF-8 -*-
 # Filename:jinxiaojian
 # Create Time: 2022/7/16 下午6:58
+"""
+需求：使用多线程实现并发下载图片
+"""
+from multiprocessing import Process, Queue
+
+import requests
+
+
+# def init_task():
+#     tasks = Queue()
+#     tasks.put("http://aaa.jpg")
+#     tasks.put("http://bbb.jpg")
+
+
+def download(q: Queue):
+    while True:
+        url = q.get()
+        r = requests.get(url)
+        img_name = url.split('/')[-1]
+        with open(img_name,"wb") as f:
+            f.write(r.content)
+        if q.empty():
+            break
+
+
+if __name__ == '__main__':
+    tasks = Queue()
+    tasks.put("https://qq.yh31.com/tp/kx/202206272059134612.jpg")
+    tasks.put("https://qq.yh31.com/tp/xg/202205032135427556.jpg")
+
+    p1 = Process(target=download, args=(tasks,))
+    p2 = Process(target=download, args=(tasks,))
+    p1.start()
+    p2.start()
